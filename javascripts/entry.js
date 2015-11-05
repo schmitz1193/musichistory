99c9fe4ad1d2917427eeb1f385aa44e816be7438
schmitz1193 <schmitz1193@gmail.com>
2015-11-05 13:49:49 -0600

    add shim in entry

--- javascripts/entry.js
+++ javascripts/entry.js
@@ -1,11 +1,16 @@
 requirejs.config({
   baseUrl: "./javascripts",
   paths:{
-    "jquery": "../lib/bower_components/jquery/dist/jquery.min"
-  }
+    "jquery": "../lib/bower_components/jquery/dist/jquery.min", 
+    "bootstrap": "../lib/bower_components/bootstrap/dist/js/bootstrap"
+  },
+
+shim: {
+	"bootstrap": ["jquery"]
+}
 });
 
-require(['songs'], 
+require(["bootstrap", 'songs'], 
 	function(songs) {
 
 });
--- javascripts/populate-songs.js
+++ javascripts/populate-songs.js
@@ -11,14 +11,31 @@ define(["jquery"], function($) {
 	//AJAX will get the songlist and then return the function with the songlist data
 	//in songs.js, the function is involkes and creates the list in the yellow box
 			$.ajax({url: "data/songs.json"})
-			.done(function(songLists) {
-				console.log("songLists ", songLists);
-				returnSongs(songLists);
-			});
+			.done(returnSongs);
 
-		}
-
+		},
 	};
 
 
 });
+
+
+
+//Review on functions with James!!!!!
+
+// doMath: function (numA, numB, mathFunction) {
+// 	return mathFunction(numA, numB);
+// }
+
+// returnSongs = function (paramenter) {
+// 	//does stuff
+// }
+
+
+//EXAMPLE FOR Passing a Function
+// var add = function (num1, num2) {
+// 	return num1 + num2
+// }
+
+
+// populate_songs.doMath(3, 4, add) 
