646570f770539e830b6dd002762454c47ebe6ccf
schmitz1193 <schmitz1193@gmail.com>
2015-12-04 09:53:29 -0600

    installed

--- app/controllers/SongCtrl.js
+++ app/controllers/SongCtrl.js
@@ -1,32 +1,45 @@
-app.controller("SongCtrl", ["$q", "$http", "$scope", function($q, $http, $scope){
+app.controller("SongCtrl", ["$q", "$http", "$scope", "songStorage", function($q, $http, $scope, songStorage){
 
 
   $scope.songs;
-    //get the data from the json file.  
-    // the data has been bound to song-list.html by the app.config(route provider) in SongApp
 
-
-  var getSongs = $q(function(resolve, reject) {
-    console.log("call to first db");
-      $http.get('./songs.json')
-      .success(
-        function(objectFromJSONFile) {
-          resolve(objectFromJSONFile.songs);
-        }, function(error) {
-          reject(error);
+      // Return the promise for controllers to listen to. This requires the following
+      // code inside the controllers
+      
+      songStorage.loadSongs().then(
+        function () {
+          $scope.songs = songStorage.getSongs();
+        },
+        function (error) {
+          console.log(error);
         }
       );
-    });
+ 
 
-// returning the promise of the call to json, 
-// if successful put the song object in $scope.songs to bind it to the DOM
+    // 
+    //get the data from the json file.  
+    // the data has been bound to song-list.html by the app.config(route provider) in SongApp
+//   var getSongs = $q(function(resolve, reject) {
+//     console.log("call to first db");
+//       $http.get('./songs.json')
+//       .success(
+//         function(objectFromJSONFile) {
+//           resolve(objectFromJSONFile.songs);
+//         }, function(error) {
+//           reject(error);
+//         }
+//       );
+//     });
 
-  getSongs.then(function (songs) {
-    console.log("songs",songs);
-    $scope.songs = songs;
-  }, function (error) {
-    console.log("Failed");
-    });
+// // returning the promise of the call to json, 
+// // if successful put the song object in $scope.songs to bind it to the DOM
+
+  // getSongs.then(function (songs) {
+  //   console.log("songs",songs);
+  //   $scope.songs = songs;
+  // }, function (error) {
+  //   console.log("Failed");
+  //   });
 
   // if the delete button is clicked call the deleteSong function to delete it
   // doesn't delete it but it does make it to the consoles message
